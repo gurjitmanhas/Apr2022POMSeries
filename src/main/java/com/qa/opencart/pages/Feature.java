c57ec268712cb4986dd011e1f373d7c3579d0abd
Naveen AutomationLabs <naveenanimation20@gmail.com>
2022-08-17 07:15:13 +0530

    feature 3

--- src/main/java/com/qa/opencart/pages/Feature.java
+++ src/main/java/com/qa/opencart/pages/Feature.java
@@ -9,6 +9,10 @@ public class Feature {
 	public void f2() {
 		System.out.println("f2 feature");
 	}
+	
+	public void f3() {
+		System.out.println("f3 feature");
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
