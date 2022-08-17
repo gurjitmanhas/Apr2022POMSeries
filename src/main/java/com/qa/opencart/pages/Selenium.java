65b33107f5b106be565af7d29054ea359b90ed64
Naveen AutomationLabs <naveenanimation20@gmail.com>
2022-08-17 07:58:36 +0530

    latest code

--- src/main/java/com/qa/opencart/pages/Selenium.java
+++ src/main/java/com/qa/opencart/pages/Selenium.java
@@ -1,14 +1,14 @@
 
 package com.qa.opencart.pages;
 
-public class TestDemo {
+public class Selenium {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		String s = "demo";
-		
 		int i = 10;
+		int p = 20;
+		
 
 	}
 
