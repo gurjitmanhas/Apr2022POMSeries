463bc41ddddf7c6656181c2aec5861fd57816d63
Naveen AutomationLabs <naveenanimation20@gmail.com>
2022-08-19 08:39:14 +0530

    added product name

--- src/main/java/com/qa/opencart/pages/LoginPage.java
+++ src/main/java/com/qa/opencart/pages/LoginPage.java
@@ -61,6 +61,7 @@ public class LoginPage {
 
 	@Step("perform search for the product: {0}")
 	public SearchResultsPage performSearch(String name) {
+		System.out.println("product name : " + name);
 		AccountsPage accPage = new AccountsPage(driver);
 		return accPage.doSearch(name);
 
